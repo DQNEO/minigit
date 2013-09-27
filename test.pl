d823c2801955f1d3e20d818d7d5b7adb23821e43
DQNEO <dqneoo@gmail.com>
2013-09-27 02:37:02 +0000

    added test script by perl

---
+++ test.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Test::More;
+
+`make >/dev/null`;
+
+my $ret1 = `./minigit -d hello_world.blob`;
+my $exp1 = "type:blob
+size: 12
+header_length:8
+hello world
+";
+
+is $ret1, $exp1;
+
+done_testing();
+
+
+
+
