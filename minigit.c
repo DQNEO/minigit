e02e0f1810a60f992ff1717c347dfbe5d8b1b9a0
DQNEO <dqneoo@gmail.com>
2013-10-03 13:47:55 +0000

    indent

--- minigit.c
+++ minigit.c
@@ -32,20 +32,20 @@ typedef struct _TAG_OBJECT_INFO {
  */
 char *sha1_to_hex(const unsigned char *sha1)
 {
-	static int bufno;
-	static char hexbuffer[4][50];
-	static const char hex[] = "0123456789abcdef";
-	char *buffer = hexbuffer[3 & ++bufno], *buf = buffer;
-	int i;
+    static int bufno;
+    static char hexbuffer[4][50];
+    static const char hex[] = "0123456789abcdef";
+    char *buffer = hexbuffer[3 & ++bufno], *buf = buffer;
+    int i;
 
-	for (i = 0; i < 20; i++) {
-		unsigned int val = *sha1++;
-		*buf++ = hex[val >> 4];
-		*buf++ = hex[val & 0xf];
-	}
-	*buf = '\0';
+    for (i = 0; i < 20; i++) {
+	unsigned int val = *sha1++;
+	*buf++ = hex[val >> 4];
+	*buf++ = hex[val & 0xf];
+    }
+    *buf = '\0';
 
-	return buffer;
+    return buffer;
 }
 
 void parse_header(char *header, object_info  *oi)
