cb103bc8ddf850d868781f037833b1b19e25ac1c
DQNEO <dqneoo@gmail.com>
2013-10-05 17:21:48 +0900

    create .git/HEAD file and the content

--- minigit.c
+++ minigit.c
@@ -573,6 +573,15 @@ int cmd_init(int argc, char *argv[])
     mkdir(".git", 0755);
     mkdir(".git/objects", 0755);
     mkdir(".git/refs", 0755);
+
+    FILE *fp;
+    if ((fp = fopen(".git/HEAD","w")) == NULL) {
+	fprintf(stderr, "unable to create .git/HEAD");
+	exit(1);
+    }
+
+    fprintf(fp, "ref: refs/heads/master\n");
+    fclose(fp);
     return 0;
 }
 
