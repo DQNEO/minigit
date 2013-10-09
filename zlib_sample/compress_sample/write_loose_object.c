016a19a8614d280b74b83cced2e3b75a1adbc16b
DQNEO <dqneoo@gmail.com>
2013-10-10 02:03:18 +0900

    sample code of zlib compression

---
+++ zlib_sample/compress_sample/write_loose_object.c
@@ -0,0 +1,118 @@
+/* zlib を使う例 */
+/* http://oku.edu.mie-u.ac.jp/~okumura/compression/comptest.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <zlib.h>
+#include <string.h>
+
+#define INBUFSIZ   1024
+#define OUTBUFSIZ  1024
+
+void _compress(FILE *fin, FILE *fout)
+{
+    z_stream z;
+    char inbuf[INBUFSIZ];
+    char outbuf[OUTBUFSIZ];
+    int count, flush, status;
+
+    /* すべてのメモリ管理をライブラリに任せる */
+    z.zalloc = Z_NULL;
+    z.zfree = Z_NULL;
+    z.opaque = Z_NULL;
+
+    /* 初期化 */
+    /* 第2引数は圧縮の度合。0〜9 の範囲の整数で，0 は無圧縮 */
+    /* Z_DEFAULT_COMPRESSION (= 6) が標準 */
+    if (deflateInit(&z, Z_DEFAULT_COMPRESSION) != Z_OK) {
+        fprintf(stderr, "deflateInit: %s\n", (z.msg) ? z.msg : "???");
+        exit(1);
+    }
+
+    z.avail_in = 0;             /* 入力バッファ中のデータのバイト数 */
+    z.next_out = (Bytef *)outbuf;        /* 出力ポインタ */
+    z.avail_out = OUTBUFSIZ;    /* 出力バッファのサイズ */
+
+    /* 通常は deflate() の第2引数は Z_NO_FLUSH にして呼び出す */
+    flush = Z_NO_FLUSH;
+
+    while (1) {
+        if (z.avail_in == 0) {  /* 入力が尽きれば */
+            z.next_in = (Bytef *)inbuf;  /* 入力ポインタを入力バッファの先頭に */
+            z.avail_in = fread(inbuf, 1, INBUFSIZ, fin); /* データを読み込む */
+
+            /* 入力が最後になったら deflate() の第2引数は Z_FINISH にする */
+            if (z.avail_in < INBUFSIZ) flush = Z_FINISH;
+        }
+        status = deflate(&z, flush); /* 圧縮する */
+        if (status == Z_STREAM_END) break; /* 完了 */
+        if (status != Z_OK) {   /* エラー */
+            fprintf(stderr, "deflate: %s\n", (z.msg) ? z.msg : "???");
+            exit(1);
+        }
+        if (z.avail_out == 0) { /* 出力バッファが尽きれば */
+            /* まとめて書き出す */
+            if (fwrite(outbuf, 1, OUTBUFSIZ, fout) != OUTBUFSIZ) {
+                fprintf(stderr, "Write error\n");
+                exit(1);
+            }
+            z.next_out = (Bytef *)outbuf; /* 出力バッファ残量を元に戻す */
+            z.avail_out = OUTBUFSIZ; /* 出力ポインタを元に戻す */
+        }
+    }
+
+    /* 残りを吐き出す */
+    if ((count = OUTBUFSIZ - z.avail_out) != 0) {
+        if (fwrite(outbuf, 1, count, fout) != count) {
+            fprintf(stderr, "Write error\n");
+            exit(1);
+        }
+    }
+
+    /* 後始末 */
+    if (deflateEnd(&z) != Z_OK) {
+        fprintf(stderr, "deflateEnd: %s\n", (z.msg) ? z.msg : "???");
+        exit(1);
+    }
+}
+
+
+void usage()
+{
+    fprintf(stderr, "Usage:\n");
+    fprintf(stderr, "  zlib -c orig_file compressed_file\n");
+    fprintf(stderr, "  zlib -d compressed_file\n");
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int c;
+    FILE *fin, *fout;
+
+    if (argc == 1) {
+	usage();
+    } else if (strcmp(argv[1],"-c") == 0) {
+        c = 1;
+    } else if (strcmp(argv[1], "-d") == 0) {
+        c = 0;
+    } else {
+	usage();
+    }
+    if ((fin = fopen(argv[2], "r")) == NULL) {
+        fprintf(stderr, "Can't open %s\n", argv[2]);
+        exit(1);
+    }
+    if (c) {
+	if ((fout = fopen(argv[3], "w")) == NULL) {
+	    fprintf(stderr, "Can't open %s\n", argv[3]);
+	    exit(1);
+	}
+      _compress(fin, fout);
+      fclose(fout);
+    } else {
+
+    }
+
+    fclose(fin);
+    return 0;
+}
