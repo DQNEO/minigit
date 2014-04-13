02853a81d0ceff54154e20431aa7754994207a1e
DQNEO <dqneoo@gmail.com>
2014-04-13 21:22:14 +0900

    kaizen

--- manualtest/01_cat-file.sh
+++ manualtest/01_cat-file.sh
@@ -6,35 +6,53 @@
 #
 # Usage:
 # [thiscommand] /path/to/.git/objetcs
+#
+# TODO バイナリファイルだとdiffが一致しないことがあるのが謎
+
+set -e
+
 obj_dir=$1
 CUR_DIR=$(cd $(dirname $0); pwd)
 MINIGIT=$CUR_DIR/../minigit
 
-find .git/objects/[^pi]* -type f | while read line
-do
-    hash=$( echo ${line#.git/objects/} | sed -e s#/##)
+if [ $# -eq 0 ] ; then
+    echo "Usage: $0  /path/to/.git/objetcs/"
+    exit 1
+fi
 
-    # compare cat-file -t
+function list_objects () {
+    find ${obj_dir}[^pi]* -type f | while read line ; do ( echo ${line#.git/objects/} | sed -e s#/##) ; done
+}
+
+
+echo "=== compare cat-file -t ===="
+list_objects | while read hash
+do
     if diff <($MINIGIT cat-file -t $hash) <(git cat-file -t $hash) ; then
         echo ok $hash
     else
         echo ng $hash
     fi
+done
 
-    # compare cat-file -s
+echo "=== compare cat-file -s ===="
+list_objects | while read hash
+do
     if diff <($MINIGIT cat-file -s $hash) <(git cat-file -s $hash) ; then
         echo ok $hash
     else
         echo ng $hash
     fi
+done
 
-    # compare cat-file -p
+echo "=== compare cat-file -p ===="
+list_objects | while read hash
+do
     if diff <($MINIGIT cat-file -p $hash) <(git cat-file -p $hash) ; then
         echo ok $hash
     else
         echo ng $hash
     fi
-
 done
 
 
