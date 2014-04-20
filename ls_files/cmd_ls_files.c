8dd4306afde6c1161ae2af8a67c42f3f8ce2d4d1
DQNEO <dqneoo@gmail.com>
2014-04-20 15:03:34 +0900

    DRY

--- ls_files/cmd_ls_files.c
+++ ls_files/cmd_ls_files.c
@@ -39,24 +39,6 @@ static inline unsigned int bswap32(unsigned int x)
 	return result;
 }
 
-char *sha1_to_hex(const unsigned char *sha1)
-{
-    static int bufno;
-    static char hexbuffer[4][50];
-    static const char hex[] = "0123456789abcdef";
-    char *buffer = hexbuffer[3 & ++bufno], *buf = buffer;
-    int i;
-
-    for (i = 0; i < 20; i++) {
-	unsigned int val = *sha1++;
-	*buf++ = hex[val >> 4];
-	*buf++ = hex[val & 0xf];
-    }
-    *buf = '\0';
-
-    return buffer;
-}
-
 int calc_padding(int n)
 {
     int floor;
