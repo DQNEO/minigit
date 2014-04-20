a897830a5f6c5c45028c1f51317816b8f501c675
DQNEO <dqneoo@gmail.com>
2014-04-20 14:32:06 +0900

    refactor

--- ls_files/cmd_ls_files.c
+++ ls_files/cmd_ls_files.c
@@ -21,28 +21,6 @@
  */
 #include "header.h"
 
-struct cache_entry {
-    unsigned int ce_ctime_sec;
-    unsigned int ce_ctime_nsec;
-    unsigned int ce_mtime_sec;
-    unsigned int ce_mtime_nsec;
-    unsigned int ce_dev;
-    unsigned int ce_ino;
-    unsigned int ce_mode;
-    unsigned int ce_uid;
-    unsigned int ce_gid;
-    unsigned int ce_size;
-    unsigned char sha1[21];
-    char namelen;
-    char name[1];
-};
-
-struct index_header {
-    char dirc[4];
-    unsigned int version;
-    unsigned int entries;
-};
-
 static inline unsigned int default_swab32(unsigned int val)
 {
 	return (((val & 0xff000000) >> 24) |
