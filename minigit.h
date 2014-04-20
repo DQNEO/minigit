7d85a59c78497ae7591cd9ad017cceaf8a4087c2
DQNEO <dqneoo@gmail.com>
2014-04-20 14:40:26 +0900

    refactor

--- minigit.h
+++ minigit.h
@@ -87,3 +87,25 @@ int _rev_parse(const char *_rev, char *sha1_string);
 int cat_commit_object(const char *sha1_string, char *parent_sha1);
 int find_file(const char *sha1_input, char *matched_filename);
 void pretty_print_tree_object(object_info *oi);
+
+struct cache_entry {
+    unsigned int ce_ctime_sec;
+    unsigned int ce_ctime_nsec;
+    unsigned int ce_mtime_sec;
+    unsigned int ce_mtime_nsec;
+    unsigned int ce_dev;
+    unsigned int ce_ino;
+    unsigned int ce_mode;
+    unsigned int ce_uid;
+    unsigned int ce_gid;
+    unsigned int ce_size;
+    unsigned char sha1[21];
+    char namelen;
+    char name[1];
+};
+
+struct index_header {
+    char dirc[4];
+    unsigned int version;
+    unsigned int entries;
+};
