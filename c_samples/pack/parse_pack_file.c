7bbe2dbb03a33f40002ce95977fe943d9117afb9
DQNEO <dqneoo@gmail.com>
2016-12-14 18:02:19 +0900

    read pack file by using struct variable

--- c_samples/pack/parse_pack_file.c
+++ c_samples/pack/parse_pack_file.c
@@ -6,25 +6,34 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 
+struct pack_header {
+    uint32_t hdr_signature;
+    uint32_t hdr_version;
+    uint32_t hdr_entries;
+};
+
+int read_pack_header(FILE *file, struct pack_header *hdr)
+{
+    fread(hdr, 12,1,file);
+    return 0;
+}
+
 void parse_file(char *filename)
 {
     FILE *file;
     file = fopen(filename, "r");
 
-    char signature[5];
-    char version[5];
-    uint32_t num;
-    uint32_t *nump = &num;
-    fread(signature, 4,1, file);
-    printf("signature=%s\n", signature); // 'PACK'
+    struct pack_header hdr;
 
-    fread(version, 4,1, file);
-    printf("version=%d\n", version[3]); 
-
-    fread(nump, 4,1, file);
-    printf("num=%d\n", ntohl(*nump)); 
+    read_pack_header(file, &hdr);
     
+    printf("signature=%d\n", hdr.hdr_signature); // 'PACK'
+    printf("version=%d\n", ntohl(hdr.hdr_version));  // 2
+    printf("num=%d\n", ntohl(hdr.hdr_entries)); 
     fclose(file);
+
+    return;
+    
 }
 
 int main(int argc, char **argv)
